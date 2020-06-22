54c2e6dcf226ddef2b633c6cc6fc03515228e96c
Sam Goddard <hello@samgoddard.co.uk>
2020-06-22 10:29:35 +0100

    new footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,33 +42,30 @@ const IndexPage = () => {
 
           <div className="mt-auto w-full">
             <motion.div variants={item}>
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-1/2 md:w-4/12 xl:w-3/12" viewBox="0 0 113 92"><g data-name="Group 79"><text data-name="Player Roberts Bell" transform="translate(0 23)" fontSize="35" fontFamily="Founders Grotesk"><tspan x="0" y="0">Player</tspan><tspan x="0" y="28">Roberts</tspan><tspan x="0" y="56">Bell</tspan></text><g data-name="Group 40" fill="none" stroke="#000"><path data-name="Path 1" d="M74.366 66.11v11.747H62.619"/><path data-name="Path 2" d="M74.366 77.857l-13.891-13.89"/></g></g></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-5/12 md:w-4/12 xl:w-3/12" viewBox="0 0 113 92"><g data-name="Group 79"><text data-name="Player Roberts Bell" transform="translate(0 23)" fontSize="35" fontFamily="Founders Grotesk"><tspan x="0" y="0">Player</tspan><tspan x="0" y="28">Roberts</tspan><tspan x="0" y="56">Bell</tspan></text><g data-name="Group 40" fill="none" stroke="#000"><path data-name="Path 1" d="M74.366 66.11v11.747H62.619"/><path data-name="Path 2" d="M74.366 77.857l-13.891-13.89"/></g></g></svg>
             </motion.div>
 
-            <motion.div variants={item}>
-              <div className="md:border-t md:border-black md:border-b">
-                <ul className="flex flex-wrap">
-                  <li className="text-xl xl:text-2xl py-3 pr-4 hidden md:block">&copy; 2020</li>
+            <motion.div variants={item}className="w-full lg:w-10/12 ml-auto mt-auto">
+              <ul className="flex flex-wrap border-t border-black border-b">
+                <li className="md:text-lg xl:text-2xl pl-0 py-2 md:py-3 px-2 block">&copy; 2020</li>
 
-                  <li className="ml-auto hidden md:block">
-                    <a className="text-xl xl:text-2xl py-3 px-4 block hover:text-white" href="mailto:hello@prb-a.com" target="_blank" rel="noopener noreferrer">hello@prb-a.com</a>
-                  </li>
+                <li className="ml-auto block border-l border-black">
+                  <a className="md:text-lg xl:text-2xl py-2 md:py-3 px-2 block hover:text-white focus:text-white hover:line-through hover:line-through" href="mailto:hello@prb-a.com" target="_blank" rel="noopener noreferrer">hello@prb-a.com</a>
+                </li>
 
-                  <li className="text-xl xl:text-2xl py-3 px-4 block border-l border-black pr-12 xl:pr-32 2xl:pr-64 hidden md:block">Architectural Design</li>
+                <li className="md:text-lg xl:text-2xl py-2 md:py-3 px-2 block border-l border-black pr-12 xl:pr-32 2xl:pr-64 hidden md:block">Architecture + Restoration</li>
 
-                  <li className="block md:hidden ml-auto">
-                    <a className="text-lg md:text-xl xl:text-2xl md:py-3 px-2 xl:px-4 block hover:text-white" href="mailto:hello@prb-a.com" target="_blank" rel="noopener noreferrer">Email</a>
-                  </li>
+                <li className="border-l border-black">
+                  <a className="md:text-lg md:text-lg xl:text-2xl py-2 md:py-3 px-2 xl:px-4 block hover:text-white focus:text-white hover:line-through hover:line-through" href="https://example.com" target="_blank" rel="noopener noreferrer">Twi<span className="hidden md:inline">tter</span></a>
+                </li>
 
-                  <li className="md:border-l md:border-black">
-                    <a className="text-lg md:text-xl xl:text-2xl md:py-3 px-2 xl:px-4 block hover:text-white" href="#" target="_blank" rel="noopener noreferrer">Twitter</a>
-                  </li>
-
-                  <li className="md:border-l md:border-black">
-                    <a className="text-lg md:text-xl xl:text-2xl md:py-3 px-2 xl:px-4 pr-0 xl:pr-0 block hover:text-white" href="#" target="_blank" rel="noopener noreferrer">Instagram</a>
-                  </li>
-                </ul>
-              </div>
+                <li className="border-l border-black">
+                  <a className="md:text-lg md:text-lg xl:text-2xl py-2 md:py-3 px-2 xl:px-4 pr-0 block hover:text-white focus:text-white hover:line-through hover:line-through" href="https://example.com" target="_blank" rel="noopener noreferrer">Insta<span className="hidden md:inline">gram</span></a>
+                </li>
+              </ul>
+              <ul className="flex flex-wrap border-b border-black md:hidden">
+                <li className="md:text-lg py-2 block pr-12 block">Architecture + Restoration</li>
+              </ul>
             </motion.div>
           </div>
         </div>
